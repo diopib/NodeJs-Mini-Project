8176aa604f319dfe0197373947b64f8fbfe4e2d5
Ibrahim Diop <diopib@gmail.com>
2011-10-10 19:26:47 -0400

    change port

--- app.js
+++ app.js
@@ -104,5 +104,6 @@ app.post(
 
 
 
-app.listen(25922);
+var port = process.env.PORT || 3000;
+app.listen(port)
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
